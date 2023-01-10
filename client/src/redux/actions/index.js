8efe373770a59a0390fd344ae828e5b5fc1d81ef
utku99 <56312196+utku99@users.noreply.github.com>
2023-01-10 19:39:33 +0300

    k

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO, UPDATE_TODO, DELETE_TODO, TOGGLE_TAB } from './type';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://utkuaksoytodo.onrender.com';
+//http://localhost:8000
 
 export const addNewTodo = (data) => async (dispatch) => {
     try {
